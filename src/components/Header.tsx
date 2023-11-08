389fbcbdd82c5dff57da4f4c274a83052b8aee1b
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2023-11-08 17:35:44 +0100

    initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+interface HeaderProps{
+
+}
+
+export default function Header(props:HeaderProps) {
+    
+
+    return(
+        <>
+        Header
+        </>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import Header from '../components/Header'
+import { Typography } from '@mui/material'
 
 
 
 export default function Main() {
 
   React.useEffect(() => {
-    
-  
+
+
     return () => {
-      
+
     }
   }, [])
-  
+
 
   return (
     <>
-    buenas tardes
+      <Header />
+      <Typography>
+        buenas tardes
+      </Typography>
     </>
   )
 }
