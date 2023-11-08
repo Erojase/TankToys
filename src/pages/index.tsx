7178da73baa79e4219177186a1d7a5fc17a9eca9
Eduardo Rojas <82035011+Erojase@users.noreply.github.com>
2023-11-08 17:19:15 +0100

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+
+export default function Main() {
+
+  React.useEffect(() => {
+    
+  
+    return () => {
+      
+    }
+  }, [])
+  
+
+  return (
+    <>
+    buenas tardes
+    </>
+  )
+}
