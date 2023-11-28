1fe8cdac2b0b7b7957ea271d5791dffbcb34f5cd
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2023-11-28 21:43:26 +0100

    add todo

--- src/components/Scope.tsx
+++ src/components/Scope.tsx
@@ -11,7 +11,7 @@ interface ScopeComponentProps{
 
 export default function TankComponent(props:ScopeComponentProps){
 
-
+    // TODO: El scope no se mueve por el trggercomonentrerender que ya no esta en mainCanvas
     React.useEffect(() => {
       
     }, [props])
@@ -22,6 +22,7 @@ export default function TankComponent(props:ScopeComponentProps){
             <img src="/mira.png"  
             style={{
                 position: 'absolute',
+                zIndex: 100,
                 width: props.width,
                 height: props.heigth, 
                 top: props.position.y - (props.width /2),
