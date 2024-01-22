7c49aabad351d92b30ba3203adae353971c1f21a
Erojase <eduardo.rojas.sanchez@alumnojoyfe.iepgroup.es>
2024-01-22 19:02:24 +0100

    add simple design functionality

--- src/app/pages/garage/garage.component.ts
+++ src/app/pages/garage/garage.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-garage',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GarageComponent implements OnInit {
 
-  constructor() { }
+  imgs: HTMLCollectionOf<HTMLImageElement>;
+  check: HTMLElement | null;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
+    this.imgs = <HTMLCollectionOf<HTMLImageElement>>document.getElementsByClassName("tankImg");
+    this.check = document.getElementById("check");
+  }
+
+  @HostListener('change', ['$event'])
+  onChange(e: Event) {
+    switch ((<HTMLElement>e.target).id) {
+      case "check":
+        this.change(e);   
+        break;
+      default:
+        break;
+    }
+  }
+
+  change(e: Event) {
+    if ((<HTMLInputElement>e.target).checked) {
+      for (const img of this.imgs) {
+        img.src = img.src.replace("top", "bot");
+      }
+    } else {
+      for (const img of this.imgs) {
+        img.src = img.src.replace("bot", "top");
+      }
+    }
+
+    let progressBars = document.getElementsByTagName("progress");
+
+    for (const bar of progressBars) {
+      bar.value = Math.random() * 100;
+    }
   }
 
 }
--- src/app/pages/signIn/signIn.component.ts
+++ src/app/pages/signIn/signIn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signIn',
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
+  login: HTMLElement | null;
+  signin: HTMLElement | null;
+
   constructor() { }
 
   ngOnInit() {
+    this.login = document.getElementById("log");
+    this.signin = document.getElementById("sign");
   }
+
+  @HostListener('click', ['$event'])
+  onClick(e:Event){
+    switch ((<HTMLElement>e.target).className) {
+      case "clickable":
+        this.hiddenToggle();
+        break;
+      default:
+        break;
+    }
+  }
+
+  hiddenToggle(){
+    if (this.login!.className.includes("hide") ) {
+      this.login!.classList.remove("hide");
+      this.signin!.classList.add("hide");
+  } else {
+      this.signin!.classList.remove("hide");
+      this.login!.classList.add("hide");
+  }
+  }
+
 
 }
